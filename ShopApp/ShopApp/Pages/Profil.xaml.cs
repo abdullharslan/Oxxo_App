f881336fb2eac44a874016fa11428965a016f026
Abdullah <87075570+abdullharslan@users.noreply.github.com>
2022-05-09 01:26:51 +0300

    Add files via upload

---
+++ ShopApp/ShopApp/Pages/Profil.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShopApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Profil : ContentPage
+    {
+        public Profil()
+        {
+            InitializeComponent();
+        }
+        private async void GirisYap(object sender, EventArgs e)
+        {
+            await TappedHome.instance.Navigation.PushAsync(new GirisYap());
+        }
+        private async void AramaButon(object sender, EventArgs e)
+        {
+            await TappedHome.instance.Navigation.PushAsync(new Arama());
+        }
+        private async void SepetButton(object sender, EventArgs e)
+        {
+            await TappedHome.instance.Navigation.PushAsync(new Sepet());
+        }
+        private async void KategoriButton(object sender, EventArgs e)
+        {
+            TappedHome.instance.CurrentPage = TappedHome.instance.Children[1];
+        }
+    }
+}
