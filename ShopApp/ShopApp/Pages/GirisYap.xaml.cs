2515ecc7912251a7994eef180bf37ce41f517c50
Abdullah <87075570+abdullharslan@users.noreply.github.com>
2022-05-09 01:24:51 +0300

    Add files via upload

---
+++ ShopApp/ShopApp/Pages/GirisYap.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShopApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GirisYap : ContentPage
+    {
+        public GirisYap()
+        {
+            InitializeComponent();
+            GirisYapStack.IsVisible = true;
+            KayitOlStack.IsVisible = false;
+        }
+        private void Giris_Clicked(object sender, EventArgs e)
+        {
+            GirisYapStack.IsVisible = true;
+            KayitOlStack.IsVisible = false;
+            GirisCizgi.BackgroundColor = Color.Black;
+            KayitCizgi.BackgroundColor = Color.Gray;
+        }
+        private void Uye_Clicked(object sender, EventArgs e)
+        {
+            GirisYapStack.IsVisible = false;
+            KayitOlStack.IsVisible = true;
+            GirisCizgi.BackgroundColor = Color.Gray;
+            KayitCizgi.BackgroundColor = Color.Black;
+        }
+        private void GeriAl(object sender, EventArgs e)
+        {
+            TappedHome.instance.Navigation.PopAsync();
+        }
+    }
+}
