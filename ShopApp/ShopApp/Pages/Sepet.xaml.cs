f4a52677cdf080f1659cb065e7720c733e862919
Abdullah <87075570+abdullharslan@users.noreply.github.com>
2022-05-09 01:27:12 +0300

    Add files via upload

---
+++ ShopApp/ShopApp/Pages/Sepet.xaml.cs
@@ -0,0 +1,48 @@
+﻿using ShopApp.Siniflar;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+namespace ShopApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Sepet : ContentPage
+    {
+        public Sepet()
+        {
+            InitializeComponent();
+            UrunleriGetir();
+            Device.StartTimer(TimeSpan.FromSeconds(5), (Func<bool>)(() =>
+            {
+                UrunleriGetir();
+                return true;
+            }));
+        }
+        async void UrunleriGetir()
+        {
+            Database database = Database.Instance();
+            var data = await database.SepetiGetir();
+            sepetsutun.ItemsSource = data;
+            sepetsutun.BindingContext = data;
+            Adet.Text = data.Count + " ADET";
+            sepetsutun.HeightRequest = data.Count * 270;
+        }
+        private async void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            ImageButton frame = (ImageButton)sender;
+            Urun img = (Urun)frame.CommandParameter;
+
+            Database database = Database.Instance();
+            await database.SepettenSil(img.UrunIsim);
+            await DisplayAlert("BAŞARILI", "ÜRÜN SEPETTEN SİLİNDİ", "TAMAM");
+            UrunleriGetir();
+        }
+        private void GeriAl(object sender, EventArgs e)
+        {
+            TappedHome.instance.Navigation.PopAsync();
+        }
+        private async void GirisYap(object sender, EventArgs e)
+        {
+            await TappedHome.instance.Navigation.PushAsync(new GirisYap());
+        }
+    }
+}
